a5bf18ec2494ce3e157f983e95e50387f89a39ae
Rayan <53921750+binelias@users.noreply.github.com>
2022-08-27 15:57:40 +0800

    leetcode 75

---
+++ Leetcode/Leetcode 75/UniquePaths.java
@@ -0,0 +1,39 @@
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+// Example 1:
+// Input: m = 3, n = 7
+// Output: 28
+
+// Example 2:
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+
+//Dynamic programming
+public class UniquePaths {
+  public int uniquePaths(int m, int n) {
+    int[][] grid = new int[n][m];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (i == 0)
+          grid[0][j] = 1;
+        if (j == 0)
+          grid[i][j] = 1;
+        if (i != 0 && j != 0) {
+          int up = grid[i - 1][j];
+          int left = grid[i][j - 1];
+          grid[i][j] = up + left;
+        }
+      }
+    }
+    return grid[n - 1][m - 1];
+  }
+}
