513b50d191f6249a1c6403118eeab63a636e0ca9
Rayan <53921750+binelias@users.noreply.github.com>
2022-08-29 18:42:57 +0800

    leetcode 75

--- Leetcode/Easy/Arrays/1.twoSum/twoSum.java
+++ Leetcode/Easy/Arrays/1.twoSum/twoSum.java
@@ -8,9 +8,10 @@ import java.util.*;
 public class twoSum {
   public int[] twoSum(int[] nums, int target) {
       HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-      for (int i = 0; i < nums.length; map.put(nums[i],i++)) 
-          if (map.containsKey(target - nums[i])) 
-              return new int[]{map.get(target - nums[i]),i};
+      for (int i = 0; i < nums.length; map.put(nums[i],i++)) {
+        if (map.containsKey(target - nums[i])) 
+          return new int[]{map.get(target - nums[i]),i};
+      }
       return new int[]{0,0};
   }
 }
--- Leetcode/Easy/Arrays/1.twoSum/twoSum.py
+++ Leetcode/Easy/Arrays/1.twoSum/twoSum.py
@@ -4,7 +4,6 @@
 # Input: nums = [2,7,11,15], target = 9
 # Output: [0,1]
 class Solution:
-  
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         matchMap = {} 
         
