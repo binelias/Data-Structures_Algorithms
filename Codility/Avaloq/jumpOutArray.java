23fbb505ae93f61b123cb4500b7a26d5391450e7
Rayan <53921750+binelias@users.noreply.github.com>
2022-08-30 17:37:37 +0800

    codilitty avaloq online assessment

--- Codility/Avaloq/jumpOutArray.java
+++ Codility/Avaloq/jumpOutArray.java
@@ -1,5 +1,29 @@
 package Avaloq;
+import java.util.*;
 
 public class jumpOutArray {
-  
+  public int solution(int[] A) {
+		int currIdx = 0;
+		int jumps = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int i = 0; i < A.length; i++) {
+			map.put(i, A[i]);
+		}
+		
+		while(currIdx >= 0 && currIdx < A.length) {
+			if(map.get(currIdx) != null) {
+				int val = map.get(currIdx);
+				map.put(currIdx, null);
+				currIdx += val;
+				jumps += 1;
+			}else {
+				return -1;
+			}
+		}
+		return jumps;
+	}
+		
+	public static void main(String[] args) {
+		System.out.println(new JumpOutArray().solution(new int[] {2, 3, -1, 1, 3}));
+	}
 }
