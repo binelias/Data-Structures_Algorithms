a241029016f332914e88dc11558c444a96bc4235
Rayan <53921750+binelias@users.noreply.github.com>
2022-09-03 13:39:35 +0800

    leetcode algorithm I

--- Leetcode/Algorithm I/TwoPointers/RotateArray.java
+++ Leetcode/Algorithm I/TwoPointers/RotateArray.java
@@ -19,5 +19,24 @@
 package Leetcode.Algorithm I.TwoPointers;
 
 public class RotateArray {
-  
+  private void reverse(int[] nums, int i, int j) {
+    int temp = 0;
+    while(i < j) {
+        temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+        j--;
+    }
+  }
+
+  public void rotate(int[] nums, int k) {
+      //corner cases
+      if(nums == null || nums.length < 2) return;
+      
+      k = k % nums.length;
+      reverse(nums, 0, nums.length - k - 1);
+      reverse(nums, nums.length - k, nums.length -1);
+      reverse(nums, 0, nums.length - 1);
+  }
 }
