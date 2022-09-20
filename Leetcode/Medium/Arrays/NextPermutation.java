08b0ad3b1cf9f908e24eac28619d71063ce988af
Rayan <53921750+binelias@users.noreply.github.com>
2022-09-20 17:53:26 +0800

    hackerrank oop

--- Leetcode/Medium/Arrays/NextPermutation.java
+++ Leetcode/Medium/Arrays/NextPermutation.java
@@ -33,37 +33,36 @@
 // Input: nums = [1,1,5]
 // Output: [1,5,1]
 
-
 class Solution {
-  //Single Pass Approach
-  public void nextPermutation(int[] nums) {
-      int i = nums.length - 2;
-      while(i >= 0 && nums[i + 1] <= nums[i]) {
-          i--;
-      }
-      if(i >= 0) {
-          int j = nums.length - 1;
-          while(nums[j] <= nums[i]) {
-              j--;
-          }
-          swap(nums, i, j);
-      }
-      reverse(nums, i + 1);
-  }//T:O(n), S:O(1)
-  
-  private void reverse(int[] nums, int start) {
-      int i = start;
-      int j = nums.length - 1;
-      while(i < j) {
-          swap(nums, i, j);
-          i++;
-          j--;
-      }
-  }
-  
-  private void swap(int[] nums, int i, int j) {
-      int temp = nums[i];
-      nums[i] = nums[j];
-      nums[j] = temp;
-  }
+    // Single Pass Approach
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }// T:O(n), S:O(1)
+
+    private void reverse(int[] nums, int start) {
+        int i = start;
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
