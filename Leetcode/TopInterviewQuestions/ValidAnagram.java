15dc0fa9e3138224f0ed8d1b2998e4567de24ba2
Rayan <53921750+binelias@users.noreply.github.com>
2022-10-04 19:33:10 +0800

    leetcode java

--- Leetcode/TopInterviewQuestions/ValidAnagram.java
+++ Leetcode/TopInterviewQuestions/ValidAnagram.java
@@ -16,26 +16,30 @@
 // s and t consist of lowercase English letters.
 
 class Solution {
-  public boolean isAnagram(String s, String t) {
-      //corner case
-      if(s == null || t == null || s.length() != t.length()) return false;
-      
-      
-      //finding count of each char in string S
-      HashMap<Character, Integer> map = new HashMap<>();
-      for(int i = 0; i < s.length(); i++) {
-          char S = s.charAt(i);
-          map.put(S, map.getOrDefault(S,0) + 1);
-      }
-      
-      //subtracting count for each character in string T, extra char found return false
-      for(int i = 0; i <s.length(); i++) {
-          char T = t.charAt(i);
-          Integer count = map.get(T);
-          if(count == null) return false;
-          if(count.equals(1)) map.remove(T);
-          else map.put(T, count - 1);
-      }
-      return true;
-  }
+    public boolean isAnagram(String s, String t) {
+        // corner case
+        if (s == null || t == null || s.length() != t.length())
+            return false;
+
+        // finding count of each char in string S
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char S = s.charAt(i);
+            map.put(S, map.getOrDefault(S, 0) + 1);
+        }
+
+        // subtracting count for each character in string T, extra char found return
+        // false
+        for (int i = 0; i < s.length(); i++) {
+            char T = t.charAt(i);
+            Integer count = map.get(T);
+            if (count == null)
+                return false;
+            if (count.equals(1))
+                map.remove(T);
+            else
+                map.put(T, count - 1);
+        }
+        return true;
+    }
 }
