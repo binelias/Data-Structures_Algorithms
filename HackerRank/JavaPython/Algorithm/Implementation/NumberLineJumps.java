894ad86e5cbc32e88857cfd4775b799a16d2985d
Rayan <53921750+binelias@users.noreply.github.com>
2022-10-25 20:28:13 +0800

    hackerrank java

---
+++ HackerRank/JavaPython/Algorithm/Implementation/NumberLineJumps.java
@@ -0,0 +1,69 @@
+// Sample Input 1
+// 0 2 5 3
+
+// Sample Output 1
+// NO
+
+// Explanation 1
+// The second kangaroo has a starting location that is ahead (further to the right) of the first kangaroo's starting location (i.e., x2 > x1 ). Because the second kangaroo moves at a faster rate (meaning v2 > v1) and is already ahead of the first kangaroo, the first kangaroo will never be able to catch up. Thus, we print NO.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+  /*
+   * Complete the 'kangaroo' function below.
+   *
+   * The function is expected to return a STRING.
+   * The function accepts following parameters:
+   * 1. INTEGER x1
+   * 2. INTEGER v1
+   * 3. INTEGER x2
+   * 4. INTEGER v2
+   */
+
+  public static String kangaroo(int x1, int v1, int x2, int v2) {
+    // Write your code here
+    if (v2 >= v1)
+      return "NO";
+    if ((x2 - x1) % (v1 - v2) == 0)
+      return "YES";
+    return "NO";
+  }
+
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+    int x1 = Integer.parseInt(firstMultipleInput[0]);
+
+    int v1 = Integer.parseInt(firstMultipleInput[1]);
+
+    int x2 = Integer.parseInt(firstMultipleInput[2]);
+
+    int v2 = Integer.parseInt(firstMultipleInput[3]);
+
+    String result = Result.kangaroo(x1, v1, x2, v2);
+
+    bufferedWriter.write(result);
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
