75f672d2e52a0f4b1fb3606996552dd7bb8429f4
Rayan <53921750+binelias@users.noreply.github.com>
2022-12-19 07:24:48 +0800

    leetcode java

---
+++ Leetcode/Easy/RangeSumQueryImmutable.java
@@ -0,0 +1,46 @@
+// Given an integer array nums, handle multiple queries of the following type:
+
+// Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+// Implement the NumArray class:
+
+// NumArray(int[] nums) Initializes the object with the integer array nums.
+// int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+// Example 1:
+
+// Input
+// ["NumArray", "sumRange", "sumRange", "sumRange"]
+// [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+// Output
+// [null, 1, -1, -3]
+
+// Explanation
+// NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+// numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+// numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+// numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+
+class NumArray {
+  int[] nums;
+
+  public NumArray(int[] nums) {
+    for (int i = 1; i < nums.length; i++) {
+      nums[i] += nums[i - 1];
+    }
+    this.nums = nums;
+  }
+
+  public int sumRange(int i, int j) {
+    if (i == 0)
+      return nums[j];
+
+    return nums[j] - nums[i - 1];
+  }
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
