6bc672c0fddf0f2a425c28ba888a85f837e47713
Rayan <53921750+binelias@users.noreply.github.com>
2022-12-24 23:40:11 +0800

    hackerrank java

---
+++ HackerRank/JavaPython/Strings/CaesarCipher.java
@@ -0,0 +1,73 @@
+// Sample Input
+
+// 11
+// middle-Outz
+// 2
+// Sample Output
+
+// okffng-Qwvb
+// Explanation
+
+// Original alphabet:      abcdefghijklmnopqrstuvwxyz
+// Alphabet rotated +2:    cdefghijklmnopqrstuvwxyzab
+
+// m -> o
+// i -> k
+// d -> f
+// d -> f
+// l -> n
+// e -> g
+// -    -
+// O -> Q
+// u -> w
+// t -> v
+// z -> b
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'caesarCipher' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. INTEGER k
+     */
+
+    public static String caesarCipher(String s, int k) {
+        // Write your code here
+        StringBuilder res = new StringBuilder();
+        for(char ch:s.toCharArray()) 
+            if('a' <= ch && ch <= 'z') 
+                res.append((char)('a'+(ch-'a'+k)%26));
+            else if('A' <= ch && ch <= 'Z') 
+                res.append((char)('A'+(ch-'A'+k)%26));
+            else 
+                res.append(ch);
+        return res.toString();
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String s = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.caesarCipher(s, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
