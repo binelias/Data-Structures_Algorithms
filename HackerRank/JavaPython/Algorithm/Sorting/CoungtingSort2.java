c97915d4ad3714f4452f9329298b9aa22024ec2f
Rayan <53921750+binelias@users.noreply.github.com>
2023-01-02 20:42:04 +0800

    hackerrank java

---
+++ HackerRank/JavaPython/Algorithm/Sorting/CoungtingSort2.java
@@ -0,0 +1,69 @@
+// Sample Input
+
+// 100
+// 63 25 73 1 98 73 56 84 86 57 16 83 8 25 81 56 9 53 98 67 99 12 83 89 80 91 39 86 76 85 74 39 25 90 59 10 94 32 44 3 89 30 27 79 46 96 27 32 18 21 92 69 81 40 40 34 68 78 24 87 42 69 23 41 78 22 6 90 99 89 50 30 20 1 43 3 70 95 33 46 44 9 69 48 33 60 65 16 82 67 61 32 21 79 75 75 13 87 70 33 
+// Sample Output
+
+// 1 1 3 3 6 8 9 9 10 12 13 16 16 18 20 21 21 22 23 24 25 25 25 27 27 30 30 32 32 32 33 33 33 34 39 39 40 40 41 42 43 44 44 46 46 48 50 53 56 56 57 59 60 61 63 65 67 67 68 69 69 69 70 70 73 73 74 75 75 76 78 78 79 79 80 81 81 82 83 83 84 85 86 86 87 87 89 89 89 90 90 91 92 94 95 96 98 98 99 99
+
+// Explanation
+
+// Once our counting array has been filled, loop from index 0 to the end, printing each i value arr[i] times.
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+  /*
+   * Complete the 'countingSort' function below.
+   *
+   * The function is expected to return an INTEGER_ARRAY.
+   * The function accepts INTEGER_ARRAY arr as parameter.
+   */
+
+  public static List<Integer> countingSort(List<Integer> arr) {
+    // Write your code here
+    int[] frequencies = new int[100];
+    Arrays.fill(frequencies, 0);
+    for (int i : arr) {
+      frequencies[i] = frequencies[i] + 1;
+    }
+    List<Integer> sorted = new ArrayList<>(arr.size());
+    for (int i = 0; i < frequencies.length; i++) {
+      int count = frequencies[i];
+      for (int j = 0; j < count; j++) {
+        sorted.add(i);
+      }
+    }
+    return sorted;
+  }
+
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        .map(Integer::parseInt)
+        .collect(toList());
+
+    List<Integer> result = Result.countingSort(arr);
+
+    bufferedWriter.write(
+        result.stream()
+            .map(Object::toString)
+            .collect(joining(" "))
+            + "\n");
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
