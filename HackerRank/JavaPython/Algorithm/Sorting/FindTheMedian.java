99afe80b8c81289d9d16d2ff5480d64244e20318
Rayan <53921750+binelias@users.noreply.github.com>
2023-01-08 21:48:20 +0800

    hackerrank java

---
+++ HackerRank/JavaPython/Algorithm/Sorting/FindTheMedian.java
@@ -0,0 +1,53 @@
+// Sample Input 0
+
+// 7
+// 0 1 2 4 6 5 3
+// Sample Output 0
+
+// 3
+// Explanation 0
+
+// The sorted arr = [0,1,2,3,4,5,6]. It's middle element is at arr[3]=3.
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+  /*
+   * Complete the 'findMedian' function below.
+   *
+   * The function is expected to return an INTEGER.
+   * The function accepts INTEGER_ARRAY arr as parameter.
+   */
+
+  public static int findMedian(List<Integer> arr) {
+    // Write your code here
+    Collections.sort(arr);
+    return arr.get(arr.size() / 2);
+  }
+
+}
+
+public class Solution {
+  public static void main(String[] args) throws IOException {
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+    List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        .map(Integer::parseInt)
+        .collect(toList());
+
+    int result = Result.findMedian(arr);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedReader.close();
+    bufferedWriter.close();
+  }
+}
