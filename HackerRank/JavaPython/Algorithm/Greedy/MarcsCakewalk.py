3898361aad0157c75691fd695447b4d5bb9a109e
Rayan <53921750+binelias@users.noreply.github.com>
2023-02-25 09:48:47 +0800

    py

---
+++ HackerRank/JavaPython/Algorithm/Greedy/MarcsCakewalk.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+
+def marcsCakewalk(calorie):
+    # Write your code here
+    return sum(c*2**i for i, c in enumerate(sorted(calorie, reverse=True)))
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
